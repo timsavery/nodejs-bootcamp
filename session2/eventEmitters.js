112b9797e80ea810a9560ec446a84350e722cf60
Tim Savery <tim.savery@gmail.com>
2013-07-10 11:09:08 -0600

    update

--- session2/eventEmitters.js
+++ session2/eventEmitters.js
@@ -1,13 +1,27 @@
-var events = require('events');
+var util = require('util'),
+		events = require('events');
 
-var emitter = new events.EventEmitter();
+/*
+ * Create a class that inherits from EventEmitter
+ */
+function MyEmitter() {
+	events.EventEmitter.apply(this);
 
-events.EventEmitter.prototype.emitSomething = function() {
-  emitter.emit('some_event', 'any data', 'that goes along with it');
-};
+	this.emitSomething = function() {
+		emitter.emit('some_event', 'any data', 'that goes along with it');
+	};
+}
+
+util.inherits(MyEmitter, events.EventEmitter);
+
+
+/*
+ * Create an instance of MyEmitter and handle events
+ */
+var emitter = new MyEmitter();
 
 emitter.on('some_event', function(data1, data2) {
-  console.log(data1 + ' ' + data2 + '\n');
+  console.log(data1 + ' ' + data2);
 
   var now = new Date();
   while (new Date().getTime() - now.getTime() < 2000);
--- session2/streams.js
+++ session2/streams.js
@@ -2,7 +2,5 @@ var http = require('http'),
     request = require('request');
 
 http.createServer(function(req, res) {
-
-  request.get('http://www.google.com').pipe(res);
-
+  request.get('http://www.google.com' + req.url).pipe(res);
 }).listen(8080);
